1905aa5abe244ff441d99aa086a9c18fa84e57b0
Аленин Андрей <aleninmailbox@gmail.com>
2022-07-16 12:01:33 +0300

    Новый профиль

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,12 @@
+
+public class FormDate {
+        public static void main(String[] args) {
+
+        }
+        public int day;
+        public int month;
+        public int year;
+
+
+}
+
---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+    public String name;
+    // добавьте другие поля
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+
+    public boolean subscription;
+
+    FormDate birthday = new FormDate();
+}
+
+
